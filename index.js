314e5f5463da6ac28229580b637ce0d901b8109d
Jim Bulkowski <jim.b@paperelectron.com>
2016-06-16 11:34:05 -0400

    Adds ability to sort plugins optional dependencies

--- index.js
+++ index.js
@@ -25,16 +25,22 @@ module.exports = function magnumTopo(pluginArray){
   var Sort = new topo()
 
   _.each(pluginArray, function(o) {
-
-    Sort.add(o.name, o.depends)
-
+    Sort.add(o.configName, o.depends)
+    Sort.add(o.configName, o.optional)
     if(o.provides) {
       var provides = _.isArray(o.provides) ? o.provides : [o.provides]
       _.each(provides, function(p) {
-        Sort.add(p, o.name)
+        Sort.add(p, o.configName)
       })
     }
   })
 
-  return Sort.sort().reverse()
+  var Sorted = Sort.sort().reverse()
+  var KeyedPlugins = _.keyBy(pluginArray, 'configName')
+  return _.chain(Sorted).map(function(key){
+    return KeyedPlugins[key]
+  })
+    .filter(_.isObject)
+    .value()
+
 }
