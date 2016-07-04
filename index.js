299fbeee1a0d2b94c7c4b7e37efecda045ee3c57
Jim Bulkowski <jim.b@paperelectron.com>
2016-07-04 00:20:19 -0400

    Correctly handle matching param names for multiple dependent plugins

--- index.js
+++ index.js
@@ -24,36 +24,52 @@ var topo = require('./lib/topo')
 module.exports = function magnumTopo(pluginArray){
   var Sort = new topo()
 
-  var keyedParams = _.chain(pluginArray).map(function(plugin) {
-    if(plugin.paramName){
-      return {paramName: plugin.paramName, configName: plugin.configName}
-    }
-    return false
-  }).filter(Boolean).keyBy('paramName').value()
-
+  var groupedParams = _.chain(pluginArray)
+    .map(function(plugin) {
+      if(plugin.paramName){
+        return {paramName: plugin.paramName, configName: plugin.configName}
+      }
+      return false
+    })
+    .filter(Boolean)
+    .groupBy('paramName')
+    .value()
 
   _.each(pluginArray, function(o) {
+
+    // Map dependencies, if there are multiple matching, return the full array
+    // and flatten at the end of the chain. This Accounts for merge plugins that
+    // all export the same paramName for use.
     var deps = _.isArray(o.depends) ? o.depends : _.isUndefined(o.depends) ? [] : [o.depends]
-    var pluginsRequiredDependencies = _.map(deps, function(d) {
-      if(_.isObject(keyedParams[d])){
-        return keyedParams[d].configName
+    var pluginsRequiredDependencies = _.chain(deps).map(function(d) {
+
+      if(_.isArray(groupedParams[d])){
+        return _.map(groupedParams[d], function(p){
+          return p.configName
+        })
       }
       return d
-    })
+    }).flatten().value()
 
+    // Same as pluginRequiredDependencies.
     var opts = _.isArray(o.optional) ? o.optional : _.isUndefined(o.optional) ? [] : [o.optional]
-    var pluginsOptionalDependencies = _.map(opts, function(d) {
-      if(_.isObject(keyedParams[d])){
-        return keyedParams[d].configName
+    var pluginsOptionalDependencies = _.chain(opts).map(function(d) {
+
+      if(_.isArray(groupedParams[d])){
+        return _.map(groupedParams[d], function(p){
+          return p.configName
+        })
       }
       return d
-    })
-
+    }).flatten().value()
 
     Sort.add(o.configName, pluginsRequiredDependencies)
     Sort.add(o.configName, pluginsOptionalDependencies)
 
 
+    // Automatically add plugins paramName, if it differs from the
+    // configName of the plugin. this allows downstream plugins to sak for dependencies
+    // by configName, moduleName, or exported parameter.
     if(o.provides) {
       var provides = _.isArray(o.provides) ? o.provides : [o.provides]
       _.each(provides, function(p) {
@@ -66,6 +82,9 @@ module.exports = function magnumTopo(pluginArray){
 
   var Sorted = Sort.sort().reverse()
   var KeyedPlugins = _.keyBy(pluginArray, 'configName')
+
+  // console.log(KeyedPlugins);
+
   return _.chain(Sorted).map(function(key){
     var byPluginName = KeyedPlugins[key]
     return byPluginName
